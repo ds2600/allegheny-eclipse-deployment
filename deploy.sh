6758be33752e66c97fca83b23f508853eaec783b
ds2600 <bill@ishiganto.com>
2025-06-13 12:28:00 -0400

    updated script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,43 @@
 #
 
 cd /var/www/allegheny-eclipse/
-git pull origin $1 
+
+LAST_VERSION_FILE=".last_version"
+VERSION="$1"
+
+VERSIONS=($(git tag | sort -V))
+
+if [ -z "$VERSION" ]; then
+    if [ -f "$LAST_VERSION_FILE" ]; then
+        LAST_VERSION=$(cat "$LAST_VERSION_FILE")
+    else
+        LAST_VERSION=""
+    fi
+
+    NEXT_VERSION=""
+    for i in "${!VERSIONS[@]}"; do
+        if [[ "${VERSIONS[$i]}" == "$LAST_VERSION" ]]; then
+            NEXT_INDEX=$((i + 1))
+            NEXT_VERSION="${VERSIONS[$NEXT_INDEX]}"
+            break
+        fi
+    done
+
+    if [ -z "$NEXT_VERSION" ]; then
+        echo "No new version to deploy."
+        exit 0
+    fi
+
+    VERSION="$NEXT_VERSION"
+fi
+
+echo "Deploying version: $VERSION"
+
+git fetch --tags
+git checkout "$VERSION" || exit 1
 composer install --no-dev --optimize-autoloader
+echo "$VERSION" > "$LAST_VERSION_FILE"
+echo "Deployment complete for version: $VERSION"
 
-
+echo "Updating WaybackMachine..."
+curl -X POST "https://web.archive.org/save/https://alleghenyeclipse.com" -H "User-Agent: Allegheny Eclipse Deployment Script"
